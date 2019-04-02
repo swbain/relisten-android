5e17cc968440ef02683b563829b3bfa022d6365f
Stephen Bain <swbain@gmail.com>
2019-04-01 19:59:32 -0500

    hello world

---
+++ app/src/main/java/com/stephenbain/relisten/ui/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.stephenbain.relisten.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.stephenbain.relisten.R
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+
+}
