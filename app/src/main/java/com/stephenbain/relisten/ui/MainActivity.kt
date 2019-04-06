976eaf0fe6d93a9459dd5692c58bacbacababdde
Stephen Bain <swbain@gmail.com>
2019-04-05 19:55:59 -0500

    setup for navigation lib

--- app/src/main/java/com/stephenbain/relisten/ui/MainActivity.kt
+++ app/src/main/java/com/stephenbain/relisten/ui/MainActivity.kt
@@ -9,7 +9,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        setContentView(R.layout.activity_main)
     }
 
 }
