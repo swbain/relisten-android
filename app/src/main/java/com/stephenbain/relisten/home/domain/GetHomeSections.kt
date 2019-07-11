90c3b1ef7eb39b39a2bc1bb6b22008663d815378
Stephen Bain <swbain@gmail.com>
2019-07-11 14:04:31 -0500

    removed dumb overuse of RX

--- app/src/main/java/com/stephenbain/relisten/home/domain/GetHomeSections.kt
+++ app/src/main/java/com/stephenbain/relisten/home/domain/GetHomeSections.kt
@@ -4,22 +4,24 @@ import com.stephenbain.relisten.home.domain.model.HomeSection
 import com.stephenbain.relisten.home.repository.Artist
 import com.stephenbain.relisten.home.repository.ArtistRepository
 import io.reactivex.Observable
+import io.reactivex.Single
 import io.reactivex.rxkotlin.Observables
 import io.reactivex.rxkotlin.toObservable
+import timber.log.Timber
 import javax.inject.Inject
 
 
 class GetHomeSections @Inject constructor(private val artistRepository: ArtistRepository) {
 
     operator fun invoke(): Observable<List<HomeSection>> {
-        return artistRepository.getArtists().flatMap(::getHomeSections)
+        return artistRepository.getArtists()
+            .doOnError { Timber.e(it, "Error getting all artists from repo") }
+            .onErrorReturnItem(emptyList())
+            .map(::getHomeSections)
     }
 
-    private fun getHomeSections(artists: List<Artist>): Observable<List<HomeSection>> {
-        return Observables.combineLatest(
-            getFeaturedArtists(artists),
-            getAllArtists(artists)
-        ).map { listOf(it.first, it.second) }
+    private fun getHomeSections(artists: List<Artist>): List<HomeSection> {
+        return listOf(getFeaturedArtists(artists), getAllArtists(artists))
     }
 
     private fun getRecentlyPlayed(): Observable<HomeSection> {
@@ -27,12 +29,8 @@ class GetHomeSections @Inject constructor(private val artistRepository: ArtistRe
         return Observable.empty()
     }
 
-    private fun getFeaturedArtists(artists: List<Artist>): Observable<out HomeSection> {
-        return artists.toObservable()
-            .filter { it.isFeatured }
-            .toList()
-            .map { HomeSection.FeaturedArtists(it) }
-            .toObservable()
+    private fun getFeaturedArtists(artists: List<Artist>): HomeSection {
+        return HomeSection.FeaturedArtists(artists.filter { it.isFeatured })
     }
 
     private fun getLatestRecordings(): Observable<HomeSection> {
@@ -40,11 +38,8 @@ class GetHomeSections @Inject constructor(private val artistRepository: ArtistRe
         return Observable.empty()
     }
 
-    private fun getAllArtists(artists: List<Artist>): Observable<out HomeSection> {
-        return artists.toObservable()
-            .toList()
-            .map { HomeSection.AllArtists(it) }
-            .toObservable()
+    private fun getAllArtists(artists: List<Artist>): HomeSection {
+        return HomeSection.AllArtists(artists)
     }
 
 }
