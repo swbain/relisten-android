ec593779f5451708c986f2b7ce3d3c7f34f8fb17
Stephen Bain <swbain@gmail.com>
2019-09-28 15:13:23 -0500

    more build fixes removing room stuff

--- app/src/main/java/com/stephenbain/relisten/common/Entities.kt
+++ app/src/main/java/com/stephenbain/relisten/common/Entities.kt
@@ -1,23 +1,8 @@
 package com.stephenbain.relisten.common
 
-import androidx.room.ColumnInfo
-import androidx.room.Embedded
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
-@Entity(tableName = "artists")
-data class Artist(
-    @PrimaryKey val id: Int,
-    val name: String,
-    @ColumnInfo(name = "is_featured") val isFeatured: Boolean
-)
+data class Artist(val id: Int, val name: String, val isFeatured: Boolean)
 
-@Entity(tableName = "shows")
 @JsonClass(generateAdapter = true)
-data class Show(
-    @PrimaryKey val id: Int,
-    @ColumnInfo(name = "display_date") @Json(name = "display_date") val displayDate: String,
-    @Embedded(prefix = "artist_") val artist: Artist
-)
+data class Show(val id: Int, val displayDate: String, val artist: Artist)
