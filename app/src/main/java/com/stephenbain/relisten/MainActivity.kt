502fb962579e2843822c034124a57b88137cda65
Stephen Bain <swbain@gmail.com>
2021-08-24 16:28:40 -0500

    added app bar

--- app/src/main/java/com/stephenbain/relisten/MainActivity.kt
+++ app/src/main/java/com/stephenbain/relisten/MainActivity.kt
@@ -3,10 +3,7 @@ package com.stephenbain.relisten
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import com.stephenbain.relisten.ui.home.HomeScreen
-import com.stephenbain.relisten.ui.theme.RelistenTheme
+import com.stephenbain.relisten.com.stephenbain.relisten.RelistenUi
 import dagger.hilt.android.AndroidEntryPoint
 
 @ExperimentalStdlibApi
@@ -15,12 +12,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            RelistenTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    HomeScreen()
-                }
-            }
+            RelistenUi()
         }
     }
 }
---
+++ app/src/main/java/com/stephenbain/relisten/RelistenUi.kt
@@ -0,0 +1,27 @@
+package com.stephenbain.relisten.com.stephenbain.relisten
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.stephenbain.relisten.R
+import com.stephenbain.relisten.ui.home.HomeScreen
+import com.stephenbain.relisten.ui.theme.RelistenTheme
+
+@Composable
+@ExperimentalStdlibApi
+fun RelistenUi() {
+    RelistenTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(color = MaterialTheme.colors.background) {
+            Column {
+                TopAppBar(title = { Text(text = stringResource(id = R.string.app_name)) })
+                HomeScreen()
+            }
+        }
+    }
+}
